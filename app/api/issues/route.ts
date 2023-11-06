18258e35acb9db9312f4de272a07a6189753daa1
Ali <aliwebdevf@gmail.com>
2023-11-06 13:17:09 +0330

    creating an API for issues

---
+++ app/api/issues/route.ts
@@ -0,0 +1,25 @@
+import z from 'zod'
+import prisma from '@/prisma/client'
+import { NextRequest, NextResponse } from 'next/server'
+
+const createIssueSchema = z.object({
+    title : z.string().min(3).max(250),
+    description : z.string().min(10)
+})
+
+export async function POST(request : NextRequest) {
+    const body = await request.json()
+    const validation = createIssueSchema.safeParse(body)
+
+    if (!validation.success)
+      return NextResponse.json(validation.error.errors, {status : 401})
+
+    const newIssue = await prisma.issue.create({
+        data : {
+            title : body.title,
+            description : body.description
+        }
+    })
+
+    return NextResponse.json(newIssue , {status : 201})
+}
