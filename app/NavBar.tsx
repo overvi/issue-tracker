d1a7942f6a3cfc29dd5bbf745600cb2c0741ff77
Ali <aliwebdevf@gmail.com>
2023-11-13 12:22:24 +0330

    Adding a drop down list for showing user details

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -6,7 +6,14 @@ import React from "react";
 import { AiFillBug } from "react-icons/ai";
 import classNames from "classnames";
 import { useSession } from "next-auth/react";
-import { Box } from "@radix-ui/themes";
+import {
+  Box,
+  Container,
+  Flex,
+  DropdownMenu,
+  Avatar,
+  Text,
+} from "@radix-ui/themes";
 
 const NavBar = () => {
   const currentPath = usePathname();
@@ -17,34 +24,59 @@ const NavBar = () => {
     { label: "Issues", herf: "/issues" },
   ];
   return (
-    <nav className="flex gap-5 p-4 border-y  items-center mb-5 ">
-      <Link href="/">
-        <AiFillBug size={18} />
-      </Link>
-      <ul className="flex gap-6 text-lg">
-        {links.map((link) => (
-          <li key={link.herf}>
-            <Link
-              className={classNames({
-                "text-zinc-900": link.herf === currentPath,
-                "text-zinc-500": link.herf !== currentPath,
-                "hover:text-zinc-800 transition-colors": true,
-              })}
-              href={link.herf}
-            >
-              {link.label}
+    <nav className=" flex gap-5 p-4 border-y mb-5 justify-between">
+      <Container>
+        <Flex align="center" justify="between" gap="5">
+          <Flex className="items-center gap-3">
+            <Link href="/">
+              <AiFillBug size={18} />
             </Link>
-          </li>
-        ))}
-      </ul>
-      <Box>
-        {status === "authenticated" && (
-          <Link href="/api/auth/signout">Log out</Link>
-        )}
-        {status === "unauthenticated" && (
-          <Link href="/api/auth/signin">Log In</Link>
-        )}
-      </Box>
+            <ul className="flex gap-6 text-lg">
+              {links.map((link) => (
+                <li key={link.herf}>
+                  <Link
+                    className={classNames({
+                      "text-zinc-900": link.herf === currentPath,
+                      "text-zinc-500": link.herf !== currentPath,
+                      "hover:text-zinc-800 transition-colors": true,
+                    })}
+                    href={link.herf}
+                  >
+                    {link.label}
+                  </Link>
+                </li>
+              ))}
+            </ul>
+          </Flex>
+          <Box>
+            {status === "authenticated" && (
+              <DropdownMenu.Root>
+                <DropdownMenu.Trigger>
+                  <Avatar
+                    radius="full"
+                    className="cursor-pointer"
+                    src={session.user?.image!}
+                    fallback="?"
+                    size="3"
+                    referrerPolicy="no-referrer"
+                  />
+                </DropdownMenu.Trigger>
+                <DropdownMenu.Content>
+                  <DropdownMenu.Label>
+                    <Text size="2">{session.user?.email!}</Text>
+                  </DropdownMenu.Label>
+                  <DropdownMenu.Item>
+                    <Link href={"api/auth/signout"}>Sign Out</Link>
+                  </DropdownMenu.Item>
+                </DropdownMenu.Content>
+              </DropdownMenu.Root>
+            )}
+            {status === "unauthenticated" && (
+              <Link href="/api/auth/signin">Log In</Link>
+            )}
+          </Box>
+        </Flex>
+      </Container>
     </nav>
   );
 };
--- app/api/auth/[...nextauth]/route.tsx
+++ app/api/auth/[...nextauth]/route.tsx
@@ -1,8 +1,8 @@
 import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
-
 import { PrismaAdapter } from "@next-auth/prisma-adapter";
 import prisma from "@/prisma/client";
+
 const handler = NextAuth({
   adapter: PrismaAdapter(prisma),
   providers: [
@@ -11,7 +11,6 @@ const handler = NextAuth({
       clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
     }),
   ],
-
   session: {
     strategy: "jwt",
   },
