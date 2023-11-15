5bc9e8d90871de6c8d2ac4067bd3f666a52f150d
Ali <aliwebdevf@gmail.com>
2023-11-15 09:32:07 +0330

    Implement Padgination

--- app/component/Padgination.tsx
+++ app/component/Padgination.tsx
@@ -1,9 +1,13 @@
+"use client";
+
 import {
   ChevronLeftIcon,
   ChevronRightIcon,
   DoubleArrowLeftIcon,
+  DoubleArrowRightIcon,
 } from "@radix-ui/react-icons";
 import { Button, Flex, Text } from "@radix-ui/themes";
+import { useRouter, useSearchParams } from "next/navigation";
 import React from "react";
 
 interface Props {
@@ -14,23 +18,52 @@ interface Props {
 
 const Padgination = ({ currentPage, totalPages, itemSize }: Props) => {
   const pages = Math.ceil(itemSize / totalPages);
+  const router = useRouter();
+  const searchParams = useSearchParams();
+
+  const updatePage = (page: number) => {
+    const params = new URLSearchParams(searchParams);
+    params.set("page", page.toString());
+    router.push("?" + params.toString());
+  };
+
   if (pages <= 1) return;
   return (
     <Flex className="gap-5 items-center">
-      <Button disabled={currentPage === 1} color="gray" variant="soft">
+      <Button
+        onClick={() => updatePage(1)}
+        disabled={currentPage === 1}
+        color="gray"
+        variant="soft"
+      >
         <DoubleArrowLeftIcon />
       </Button>
-      <Button disabled={currentPage === 1} color="gray" variant="soft">
+      <Button
+        onClick={() => updatePage(currentPage - 1)}
+        disabled={currentPage === 1}
+        color="gray"
+        variant="soft"
+      >
         <ChevronLeftIcon />
       </Button>
       <Text size="2">
         page {currentPage} of {pages}
       </Text>
-      <Button disabled={currentPage === pages} color="gray" variant="soft">
+      <Button
+        onClick={() => updatePage(currentPage + 1)}
+        disabled={currentPage === pages}
+        color="gray"
+        variant="soft"
+      >
         <ChevronRightIcon />
       </Button>
-      <Button disabled={currentPage === pages} color="gray" variant="soft">
-        <DoubleArrowLeftIcon />
+      <Button
+        onClick={() => updatePage(pages)}
+        disabled={currentPage === pages}
+        color="gray"
+        variant="soft"
+      >
+        <DoubleArrowRightIcon />
       </Button>
     </Flex>
   );
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,17 @@
 import Padgination from "./component/Padgination";
 
-export default function Home() {
+export default function Home({
+  searchParams,
+}: {
+  searchParams: { page: string };
+}) {
   return (
     <div>
-      <Padgination currentPage={2} itemSize={100} totalPages={10} />
+      <Padgination
+        currentPage={parseInt(searchParams.page)}
+        itemSize={100}
+        totalPages={10}
+      />
     </div>
   );
 }
