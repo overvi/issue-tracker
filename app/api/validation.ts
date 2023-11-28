11faf904fe9366fbb8fa0e7e72f08d1909001e75
Ali <aliwebdevf@gmail.com>
2023-11-28 20:51:53 +0330

    remove sentry

--- app/api/validation.ts
+++ app/api/validation.ts
@@ -1,27 +1,26 @@
-import  { z } from 'zod'
+import { z } from "zod";
 
 export const createIssueSchema = z.object({
-    title : z.string().min(1 , 'Title is required').max(250),
-    description : z.string().min(1 , 'Description is Required')
-})
+  title: z.string().min(1, "Title is required").max(250),
+  description: z.string().min(1, "Description is Required"),
+});
 export const PatchIssueSchema = z.object({
-    title : z.string()
-    .min(1 , 'Title is required')
-    .max(250).
-    optional(),
-    description : 
-    z.string()
-    .min(1 , 'Description is Required')
+  title: z.string().min(1, "Title is required").max(250).optional(),
+  description: z
+    .string()
+    .min(1, "Description is Required")
     .max(65535)
     .optional(),
-    assignedToUserId :z.string().min(1 , "AssignedToUserId is required")
+  assignedToUserId: z
+    .string()
+    .min(1, "AssignedToUserId is required")
     .max(255)
     .optional()
-    .nullable()
-})
+    .nullable(),
+});
 
-const Validation = (body : any) => {
-    return createIssueSchema.safeParse(body)
-}
+const Validation = (body: any) => {
+  return createIssueSchema.safeParse(body);
+};
 
-export default Validation
+export default Validation;
