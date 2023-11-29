dfe2730cf119cd758fecbfea06c75f6e4c55c294
Ali <aliwebdevf@gmail.com>
2023-11-29 13:03:35 +0330

    reset database

--- prisma/migrations/20231129093321_migrate/migration.sql
+++ prisma/migrations/20231129093321_migrate/migration.sql
@@ -1,7 +1,7 @@
 -- CreateTable
 CREATE TABLE `Issue` (
     `id` INTEGER NOT NULL AUTO_INCREMENT,
-    `title` VARCHAR(250) NOT NULL,
+    `title` VARCHAR(255) NOT NULL,
     `description` TEXT NOT NULL,
     `status` ENUM('OPEN', 'IN_PROGRESS', 'CLOSED') NOT NULL DEFAULT 'OPEN',
     `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
