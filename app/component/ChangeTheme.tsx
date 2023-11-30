3a99a4b492d02ed06c80bb60fa3c6f7cf866e144
Ali <aliwebdevf@gmail.com>
2023-11-30 08:19:32 +0330

    Add ColorMode to localstorage

--- app/component/ChangeTheme.tsx
+++ app/component/ChangeTheme.tsx
@@ -1,26 +1,36 @@
 "use client";
 import { IconButton } from "@radix-ui/themes";
+import { useEffect } from "react";
 import { FaRegLightbulb, FaRegMoon } from "react-icons/fa";
 import { IoCloudyNightOutline } from "react-icons/io5";
 import { create } from "zustand";
 
 interface DarkLight {
-  colorMode: "light" | "dark";
-  setColorMode: (color: "light" | "dark") => void;
+  colorMode: string;
+  setColorMode: (color: string) => void;
 }
 
 export const useColorMode = create<DarkLight>((set) => ({
   colorMode: "light",
-  setColorMode: (color: "light" | "dark") => set({ colorMode: color }),
+  setColorMode: (color: string) => set({ colorMode: color }),
 }));
 
 const ChangeTheme = () => {
   const { setColorMode, colorMode } = useColorMode();
   const icon = colorMode === "light" ? <FaRegMoon /> : <FaRegLightbulb />;
+  const setColorLogic = colorMode === "light" ? "dark" : "light";
+
+  useEffect(() => {
+    const savedTheme = localStorage.getItem("theme");
+    setColorMode(savedTheme ?? "dark");
+  }, []);
 
   return (
     <IconButton
-      onClick={() => setColorMode(colorMode === "light" ? "dark" : "light")}
+      onClick={() => {
+        setColorMode(setColorLogic);
+        localStorage.setItem("theme", setColorLogic);
+      }}
       variant="ghost"
     >
       <IoCloudyNightOutline size="25" />
